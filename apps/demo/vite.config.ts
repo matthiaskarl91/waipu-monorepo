ee7839acb1bfe89bea7389c2072609ba18bc80df
Matthias Karl <matthias.karl@exaring.de>
2025-11-17 14:41:53 +0100

    fix: demo app

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,6 +1,6 @@
 /// <reference types='vitest' />
 import { defineConfig } from 'vite';
-import { reactRouter } from '@react-router/dev/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => ({
   root: __dirname,
@@ -13,7 +13,7 @@ export default defineConfig(() => ({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [!process.env.VITEST && reactRouter()],
+  plugins: [react()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [],
