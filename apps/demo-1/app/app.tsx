fd00448776c53276c0620759ed9ffae40ae1fd35
Matthias Karl <matthias.karl@exaring.de>
2025-11-21 15:29:21 +0100

    setup astro with react router

---
+++ apps/demo-1/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@waipu-monorepo/demo-1" />
+    </div>
+  );
+}
+
+export default App;
