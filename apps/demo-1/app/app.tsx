bbb4e765952b93944c3578f8882383ededb73db9
Matthias Karl <matthias.karl@exaring.de>
2025-11-24 09:55:14 +0100

    utilize nx

--- apps/demo-1/app/app.tsx
+++ apps/demo-1/app/app.tsx
@@ -3,6 +3,7 @@ import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <div>
+      Version: <code>1.0.0</code>
       <NxWelcome title="@waipu-monorepo/demo-1" />
     </div>
   );
